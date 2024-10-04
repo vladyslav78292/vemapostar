95864285ca1a25b5ee12071b47bcd763c47dc81f
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2024-10-04 10:27:59 +0300

    vemabet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,91 +1,54 @@
 'use strict';
 
-$(document).ready(function(){
-  $(".MainSliderCarousel").slick({
-    rtl:false,
-    autoplay:true,
-    speed: 500,
-    dots: false,
-    autoplaySpeed: 4000,
-    slidesToShow:1,
-    slidesToScroll:1,
-    pauseOnHover:true,
-    appendArrows:$(".SliderArrows"),
-    prevArrow:'<span class="Slider-Slick-Prev"></span>',
-    nextArrow:'<span class="Slider-Slick-Next"></span>',
-    easing:"linear",
-    responsive:[
-      {breakpoint:721,settings:{
-        dots: true,
-        appendArrows:$(".SliderArros"),
-      }},
-    ],
-  })
+let counter = 0;
+
+for (const menu of document.querySelectorAll(".content-menu")) {
+  menu.addEventListener("click", () => {
+      menu.querySelector(".menu-text").classList.toggle("menu-text-open");
+      menu.querySelector(".list_titles").classList.toggle("active_menu");
+      if (counter % 2 === 0) {
+        menu.classList.add("active");
+      } else {
+        menu.classList.remove("active");
+      }
+      counter = counter + 1;
+      menu.querySelector(".menu-content").classList.toggle(
+          "menu-content-hidden"
+      );
+      console.log(counter)
+  });
+}
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li');
+
+  select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    menu.classList.toggle('menu-open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      select.classList.remove('select-clicked');
+      menu.classList.remove('menu-open');
+    });
+  });
 });
 
-$(".question-wrapper").click( function () {
-  var container = $(this).parents(".accordion");
-  var answer = container.find(".answer-wrapper");
-  var trigger = container.find(".material-icons.drop");
-	var state = container.find(".question");
-  
-  answer.animate({height: "toggle"}, 100);
-  
-  if (trigger.hasClass("icon-expend")) {
-    trigger.removeClass("icon-expend");
-		state.removeClass("active");
-  }
-  else {
-    trigger.addClass("icon-expend");
-		state.addClass("active");
-  }
-  
-  if (container.hasClass("expanded")) {
-    container.removeClass("expanded");
-  }
-  else {
-    container.addClass("expanded");
-  }
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
 
-  if (container.hasClass("expanded")) {
-    container.removeClass('border');
+openMobileMenuBtn.addEventListener("click", () => {
+  if(openMobileMenuBtn.classList.contains("open")) {
+    openMobileMenuBtn.classList.remove("open");
+    headerMenu.classList.remove("active");
   } else {
-    container.addClass('border');
+    headerMenu.classList.add("active");
+    openMobileMenuBtn.classList.add("open");
   }
-});
-
-const button = document.getElementById('burger-menu');
-const menu = document.getElementById('menu');
-const closedMenu = document.getElementById('close_menu');
-const body = document.getElementById('body');
-const button_top = $('#button');
-const header = document.getElementById('header');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300 && !body.hasOwnProperty('open')) {
-    button_top.addClass('show');
-    header.classList.add('header__background');
-  } else {
-    button_top.removeClass('show');
-    header.classList.remove('header__background');
-  }
-});
-
-button_top.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-button.addEventListener('click', () => {
-  menu.classList.add('burger-menu--open');
-  menu.classList.remove('burger-menu--closed');
-  body.classList.add('open');
-  button_top.removeClass('show');
-  
-});
-
-closedMenu.addEventListener('click', () => {
-  menu.classList.remove('burger-menu--open');
-  body.classList.remove('open');
-  menu.classList.add('burger-menu--closed');
-});
+})
